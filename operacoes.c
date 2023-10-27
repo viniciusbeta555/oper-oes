05eaaab0c519cfad372a88568d33e527904e2189
viniciusbeta555 <96143281+viniciusbeta555@users.noreply.github.com>
2023-10-27 16:50:25 -0300

    Add files via upload

--- operacoes.c
+++ operacoes.c
@@ -1,40 +1,39 @@
+// ALUNO: Francisco Vinicius de Freitas Carneiro
+
 #include <stdio.h>
 
 int main (){
     int vet[6];
     int i;
     float soma = 0, media;
-    char resp;
+    char resp = 'S';
 
-    resp = 'S';
-
-while(resp == 'S' || resp == 's'){
+while(resp == 'S' || resp == 's'){  // Condição para repetir o programa
 
     for(i=0; i<6; i++){
         printf("Digite um numero: ");
         scanf("%d", &vet[i]);
     }
 
-    printf("[");
+    printf("[");  // Mostar os valores na ordem digitada
     for(i=0; i<6; i++){
         if(i==5){
-            printf("%d", vet[i]);
+            printf("%d]\n", vet[i]);
         }else{
            printf("%d,", vet[i]);  
         }
     }
 
-    for(i=6; i<0; i--){
-        if(i==1){
-            printf("%d", vet[i]);
+    printf("[");  // Mostrar os valores na ordem inversa
+    for(i=5; i>-1; i--){
+        if(i==0){
+            printf("%d]\n", vet[i]);
         }else{
            printf("%d,", vet[i]);  
         }
     }
 
-    printf("]\n");
-
-    for (i=0; i<6; i++){
+    for (i=0; i<6; i++){  // Realiza a soma dos numeros do vetor
         soma = soma+vet[i];
     }
 
@@ -43,10 +42,10 @@ while(resp == 'S' || resp == 's'){
     printf("A soma e: %.0f\n",soma);
     printf("A media e: %.2f\n", media);
 
-    soma = '\0';
+    soma = '\0';  // Zera as variaveis caso o usuario queira repetir o processo
     media = '\0';
 
-    printf("[");
+    printf("[");  // Exibe os numero pares
     for(i=0; i<6; i++){
         if(vet[i]%2 == 0){
             if(i==5){
